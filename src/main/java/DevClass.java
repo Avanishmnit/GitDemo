6116e15ee92d53a03bc5d54d843e27f759abb929
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 14:26:44 +0530

    Changed in dev class

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Avanish first Project");
-        System.out.println("Chamnges in branch1");
-        System.out.println("Feature branch");
+        System.out.println("this is bug fix");
+
     }
 }
