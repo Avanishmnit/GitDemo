be9af9858b8ae29cb6591de9d79ccb34ca2cafea
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 13:46:17 +0530

    Add doc

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Avanish first Project");
         System.out.println("Chamnges in branch1");
+        System.out.println("This is documentation");
     }
 }
