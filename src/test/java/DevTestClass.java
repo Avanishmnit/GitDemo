8863a3f3f080d634efe24453c02ba7e3f4315340
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 11:40:43 +0530

    changedin test

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -1,5 +1,6 @@
 public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("Avanish Test Calss");
+        System.out.println("Avanis Changed Something");
     }
 }
