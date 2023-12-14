428881f4a4f76099bdf2172cb24a3fae40541283
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 14:43:58 +0530

    messagt

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,7 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Avanish first Project");
         System.out.println("this is bug fix");
+        System.out.println("Welocme to our website");
 
     }
 }
