bf7748241b6b6c330c96aa0b83376ca33ef61809
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 11:58:16 +0530

    Chenged in deoClass

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,5 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Avanish first Project");
+        System.out.println("Chamnges in branch1");
     }
 }
