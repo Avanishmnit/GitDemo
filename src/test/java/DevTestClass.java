b3cb9bc3f9b59672f65be1142b35b8960b229ec7
Avanishmnit <avanishmnit@gmail.com>
2023-12-14 11:49:34 +0530

    Changed in test

--- src/test/java/DevTestClass.java
+++ src/test/java/DevTestClass.java
@@ -2,5 +2,6 @@ public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("Avanish Test Calss");
         System.out.println("Avanis Changed Something");
+        System.out.println("Avanish Second change");
     }
 }
